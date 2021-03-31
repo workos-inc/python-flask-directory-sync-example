c02a9fb3fb3895d1671b490348828b13716fad71
erin@workos.com <erin@workos.com>
2021-03-30 18:03:57 -0700

    Small refactor for readability

--- app.py
+++ app.py
@@ -3,24 +3,21 @@ import os
 import flask
 import workos
 
-# Flask Setup
 DEBUG = False
 app = flask.Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-# WorkOS Setup
 workos.api_key = os.getenv('WORKOS_API_KEY')
-workos.base_api_url = 'http://localhost:7000/' if DEBUG else workos.base_api_url
-DIRECTORY = os.getenv('SCIM_ENDPOINT_ID')
+workos.base_api_url = 'http://localhost:5000/' if DEBUG else workos.base_api_url
+directory_id = os.getenv('DIRECTORY_ID')
 
 
 @app.route('/users')
 def directory_users():
-    users = workos.client.directory_sync.list_users(directory=DIRECTORY)
+    users = workos.client.directory_sync.list_users(directory=directory_id)
     return users
 
 
 @app.route('/groups')
 def directory_groups():
-    groups = workos.client.directory_sync.list_groups(directory=DIRECTORY)
+    groups = workos.client.directory_sync.list_groups(directory=directory_id)
     return groups
